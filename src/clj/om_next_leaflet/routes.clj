51aa6ff57f62a07c3ff21347a1fef028b72827a3
ponkore <ponkore@gmail.com>
2017-12-30 18:21:59 +0900

    switch from :compojure to :bidi

--- src/clj/om_next_leaflet/routes.clj
+++ src/clj/om_next_leaflet/routes.clj
@@ -1,33 +1,41 @@
 (ns om-next-leaflet.routes
   (:require [clojure.java.io :as io]
-            [compojure.core :refer [ANY GET PUT POST DELETE routes]]
-            [compojure.route :refer [resources not-found]]
-            [ring.util.response :refer [response]]
+            [bidi.bidi :as bidi]
+            [bidi.ring :refer [resources]]
+            [ring.util.response :refer [response resource-response]]
+            [taoensso.timbre :as timbre :refer [log trace debug info warn error fatal]]
             [cheshire.core :refer [generate-string]]
             [om-next-leaflet.geojson :as geojson]
             [om.next.server :as om]))
+
+(defn index-handler
+  [req]
+  (assoc (resource-response "index.html" {:root "public"})
+         :headers {"Content-Type" "text/html; charset=UTF-8"}))
 
 (defn generate-response-json [data & [status]]
   {:status  (or status 200)
    :headers {"Content-Type" "application/json; charset=UTF-8"}
    :body    (generate-string data)})
 
-(def state (atom {:app/title "initial server title"}))
+(defn lines-handler
+  [req]
+  (generate-response-json
+   (->> (geojson/get-lines)
+        (map (fn [{:keys [id line-name bounding-box geometry]}] [id line-name bounding-box geometry])))))
 
-(defn home-routes [endpoint]
-  (routes
-   (GET "/" _
-     (-> "public/index.html"
-         io/resource
-         io/input-stream
-         response
-         (assoc :headers {"Content-Type" "text/html; charset=utf-8"})))
-   (GET "/api2/lines" _
-     (generate-response-json
-      (->> (geojson/get-lines)
-           (map (fn [{:keys [id line-name bounding-box geometry]}] [id line-name bounding-box geometry])))))
-   (GET "/api2/lines/:line-id/stations" [line-id]
-     (generate-response-json
-      (geojson/get-stations (fn [m] (= (:line-id m) (Integer/parseInt line-id))))))
-   (resources "/")
-   (not-found "Not Found")))
+(defn stations-handler
+  [req]
+  (generate-response-json
+   (geojson/get-stations (fn [m] (= (:line-id m) (Integer/parseInt (get-in req [:params :line-id])))))))
+
+(def route ["/" {"" {:get index-handler}
+                 "index.html" {:get index-handler}
+                 "api2/lines" {"" {:get lines-handler}
+                               ["/" [#"\d+" :line-id]] {"/stations" {:get stations-handler}}}
+                 "css" {:get (resources {:prefix "public/css/"})}
+                 "js" {:get (resources {:prefix "public/js/"})}}])
+
+(defn home-routes
+  [endpoint]
+  route)
